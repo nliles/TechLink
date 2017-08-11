ec0a80f6075b43d01b96cf35a63c63ae876378f4
Natalie_Liles <Natalieliles@gmail.com>
2017-08-11 10:18:18 -0400

    Refactor /jobs.json GET request to use fetch instead of ajax.

--- client/src/components/JobList.js
+++ client/src/components/JobList.js
@@ -13,25 +13,25 @@ class JobList extends Component {
 
 	componentDidMount() {
 		const self = this;
-	   $.ajax({
-	      url: '/jobs',
-	      dataType: 'json',
-	      type: 'GET',
-	      success: (data) => {
-	        this.setState({jobs: data});
-	      },
-	      error: (xhr, status, err) => {
-	        this.setState({data: err});
-	       console.error(self.props.url, status, err.toString());
-	      }
-	    });
+			fetch('/jobs.json', {
+			  method: 'GET',
+			  headers: {
+			    Accept: 'application/json',
+			  },
+			},
+			).then(response => {
+			  if (response.ok) {
+			    response.json().then(json => {
+			      this.setState({jobs: json});
+			    });
+			  }
+			});
 	}
 
 	render() {
 		return(
 	      <div className="jobList">
 	        <h2 className="activity">Job Activity</h2><br/>
-
 	        	<div className="jobs">
 	        		{this.state.jobs.map((value, key) => {
 	        			return (
