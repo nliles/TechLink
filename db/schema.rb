fe112468cbcb02d54f47ffb00a230c6f7e2b4a5b
Natalie_Liles <Natalieliles@gmail.com>
2017-08-17 13:45:22 -0400

    Fix auth_token issue.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817053152) do
+ActiveRecord::Schema.define(version: 20170817173954) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "position"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170817053152) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
