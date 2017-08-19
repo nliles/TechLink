a0a060f041df28316ee82220a5e8399004bc6856
Natalie_Liles <Natalieliles@gmail.com>
2017-08-18 22:23:59 -0400

    Refactor.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds 
   respond_to :json
 
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+
   include Authenticable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :jobs
   end
+
+	get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+	  !request.xhr? && request.format.html?
+	end
+
 end
 
